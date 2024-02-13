97917dc139f7c2608d9a6f247ea136328c1ef685
peg <ameba23@systemli.org>
2024-02-13 10:20:01 +0100

    Error handle

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ async fn main() {
     let app = Router::new()
         .route("/programs", get(list_programs))
         .route("/program/:program_hash", get(get_program))
-        .route("/build-tar", post(build_tar))
         .route("/build-git", post(build_git))
+        .route("/build-tar", post(build_tar))
         .with_state(AppState {
             db: sled::open("./db").unwrap(),
         });
@@ -42,35 +42,22 @@ async fn main() {
 }
 
 /// Get metadata about a program with a given hash
-async fn get_program(State(state): State<AppState>, Path(program_hash): Path<String>) -> String {
-    let hash = hex::decode(program_hash).unwrap();
-    std::str::from_utf8(&state.db.get(hash).unwrap().unwrap())
-        .unwrap()
-        .to_string()
+async fn get_program(
+    State(state): State<AppState>,
+    Path(program_hash): Path<String>,
+) -> Result<String, AppError> {
+    let hash = hex::decode(program_hash)?;
+    Ok(std::str::from_utf8(&state.db.get(hash)?.ok_or(AppError::ProgramNotFound)?)?.to_string())
 }
 
 /// Get hashes of all programs in the db
-async fn list_programs(State(state): State<AppState>) -> String {
+async fn list_programs(State(state): State<AppState>) -> Result<String, AppError> {
     let mut hashes = Vec::new();
     for res in state.db.iter() {
-        let (key, _value) = res.unwrap();
+        let (key, _value) = res?;
         hashes.push(hex::encode(key));
     }
-    serde_json::to_string(&hashes).unwrap()
-}
-
-/// Add a program given as a tar achive
-async fn build_tar(
-    State(state): State<AppState>,
-    input: Bytes,
-) -> Result<(StatusCode, String), AppError> {
-    let input = input.to_vec();
-    let mut archive = Archive::new(&input[..]);
-    let temp_dir = TempDir::new().unwrap();
-    archive.unpack(temp_dir.path())?;
-
-    set_current_dir(temp_dir.path())?;
-    build(state).await
+    Ok(serde_json::to_string(&hashes)?)
 }
 
 /// Add a program given as a location of a git repo
@@ -78,7 +65,7 @@ async fn build_git(
     State(state): State<AppState>,
     git_url: String,
 ) -> Result<(StatusCode, String), AppError> {
-    let temp_dir = TempDir::new().unwrap();
+    let temp_dir = TempDir::new()?;
     let output = Command::new("git")
         .arg("clone")
         .arg("--depth=1")
@@ -89,10 +76,24 @@ async fn build_git(
         .output()?;
 
     if !output.status.success() {
-        return Err(AppError::CompilationFailed(
+        return Err(AppError::GitClone(
             String::from_utf8_lossy(&output.stderr).to_string(),
         ));
     }
+
+    set_current_dir(temp_dir.path())?;
+    build(state).await
+}
+
+/// Add a program given as a tar achive
+async fn build_tar(
+    State(state): State<AppState>,
+    input: Bytes,
+) -> Result<(StatusCode, String), AppError> {
+    let input = input.to_vec();
+    let mut archive = Archive::new(&input[..]);
+    let temp_dir = TempDir::new()?;
+    archive.unpack(temp_dir.path())?;
 
     set_current_dir(temp_dir.path())?;
     build(state).await
@@ -104,9 +105,11 @@ async fn build(state: AppState) -> Result<(StatusCode, String), AppError> {
     let metadata = MetadataCommand::new()
         .manifest_path("./Cargo.toml")
         .features(CargoOpt::AllFeatures)
-        .exec()
-        .unwrap();
-    let root_package_metadata = metadata.root_package().unwrap();
+        .exec()?;
+
+    let root_package_metadata = metadata
+        .root_package()
+        .ok_or(AppError::MetadataMissingRootPackage)?;
 
     // Get the docker image name from Cargo.toml, if there is one
     let docker_image_name = get_docker_image_name_from_metadata(&root_package_metadata.metadata);
@@ -134,7 +137,7 @@ async fn build(state: AppState) -> Result<(StatusCode, String), AppError> {
 
     // Get the hash of the binary
     let hash = {
-        let binary_filename = get_binary_filename().await.unwrap();
+        let binary_filename = get_binary_filename().await?;
         let mut file = File::open(binary_filename).await?;
         let mut contents = vec![];
         file.read_to_end(&mut contents).await?;
@@ -143,44 +146,36 @@ async fn build(state: AppState) -> Result<(StatusCode, String), AppError> {
     };
 
     // Write metadata to db
-    let root_package_metadata_json = serde_json::to_string(&root_package_metadata).unwrap();
+    let root_package_metadata_json = serde_json::to_string(&root_package_metadata)?;
     state
         .db
-        .insert(hash, root_package_metadata_json.as_bytes())
-        .unwrap();
+        .insert(hash, root_package_metadata_json.as_bytes())?;
 
     // TODO Make the binary itself available
     Ok((StatusCode::OK, format!("{:?}", hash)))
 }
 
-#[derive(Debug, Error)]
-enum AppError {
-    #[error("Io error: {0}")]
-    Io(#[from] std::io::Error),
-    #[error("Compilation failed: {0}")]
-    CompilationFailed(String),
-}
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        let body = format!("{self}").into_bytes();
-        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
-    }
-}
-
 /// Get the name of the first .wasm file we find in the target directory
-async fn get_binary_filename() -> Option<PathBuf> {
-    let mut dir_contents = read_dir("binary-dir").await.unwrap();
-    while let Some(entry) = dir_contents.next_entry().await.unwrap() {
+async fn get_binary_filename() -> Result<PathBuf, AppError> {
+    let mut dir_contents = read_dir("binary-dir").await?;
+    while let Some(entry) = dir_contents.next_entry().await? {
         if let Some(extension) = entry.path().extension() {
             if extension.to_str() == Some("wasm") {
-                return Some(entry.path());
+                return Ok(entry.path());
             }
         }
     }
-    None
+    Err(AppError::CompilationFailed(
+        "Cannot find binary after compiling".to_string(),
+    ))
 }
 
+/// We expect there to be a docker image given in the Cargo.toml file like so:
+/// ```toml
+/// [package.metadata.entropy-program]
+/// docker-image = "peg997/build-entropy-programs:version0.1"
+/// ```
+/// If this is not present a default image is used
 fn get_docker_image_name_from_metadata(metadata: &serde_json::value::Value) -> Option<String> {
     if let serde_json::value::Value::Object(m) = metadata {
         if let Some(serde_json::value::Value::Object(p)) = m.get("entropy-program") {
@@ -190,4 +185,35 @@ fn get_docker_image_name_from_metadata(metadata: &serde_json::value::Value) -> O
         }
     }
     None
+}
+
+#[derive(Debug, Error)]
+enum AppError {
+    #[error("Could not clone git repository: {0}")]
+    GitClone(String),
+    #[error("Cannot find root package in Cargo.toml")]
+    MetadataMissingRootPackage,
+    #[error("Error reading Cargo.toml: {0}")]
+    Metadata(#[from] cargo_metadata::Error),
+    #[error("JSON error: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("Utf8Error: {0}")]
+    Utf8(#[from] std::str::Utf8Error),
+    #[error("Database error {0}")]
+    Db(#[from] sled::Error),
+    #[error("Cannot decode hex {0}")]
+    Hex(#[from] hex::FromHexError),
+    #[error("Io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Compilation failed: {0}")]
+    CompilationFailed(String),
+    #[error("Program not found")]
+    ProgramNotFound,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let body = format!("{self}").into_bytes();
+        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+    }
 }
